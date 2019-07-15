ebbb3720505f9911558b8f731f242252988bb045
Julian <thezhukeeper@gmail.com>
2019-07-15 12:34:16 -0700

    Add class cleanup and socket script change

--- app.js
+++ app.js
@@ -10,26 +10,16 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 
-// express to pull client files?
 app.use("/client", express.static(__dirname + "/client"));
 serv.listen(2000);
 
-Tree.list = {};
-Player.list = {};
-
 const socketList = {};
 
-const playerTreeCollision = (pos, size) => {
-  return Object.values(Tree.list).some(tree => {
-    const dx = pos.x - tree.x;
-    const dy = pos.y - tree.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-
-    if (distance < size + tree.size) {
-      return true;
-    }
-  });
-};
+const entities = {
+  players: Player.list,
+  trees: Tree.list,
+  // ghosts: Ghost.list
+} 
 
 Player.onConnect = socket => {
   const id = Object.keys(socketList).length;
@@ -49,10 +39,6 @@ Player.onDisconnect = socket => {
   delete Player.list[socket.id];
 };
 
-// spawn trees. how do we add/remove from entities list?
-// player/tree interactions
-// circular collision
-
 // on client "connection", do the following
 io.sockets.on("connection", socket => {
   socket.id = Math.random();
@@ -67,71 +53,16 @@ io.sockets.on("connection", socket => {
   });
 });
 
-// handles all updates for all players
-// returns pack of all updates as array
-// make .update for all classes
-
-Player.update = () => {
-  const pack = [];
-
-  for (let i in Player.list) {
-    let player = Player.list[i];
-
-    let newX = player.x;
-    let newY = player.y;
-
-    if (player.pressingRight) newX += player.speed;
-    if (player.pressingLeft) newX -= player.speed;
-    if (player.pressingUp) newY -= player.speed;
-    if (player.pressingDown) newY += player.speed;
-
-    // if new position doesn't cause collision, update player
-    if (!playerTreeCollision({ x: newX, y: newY}, player.size)) {
-      player.update();
-    }
-    
-    // player.update();
-    pack.push(player);
-  }
-
-  return pack;
-}
-
-// add to a spawn file
-Tree.spawnTrees = () => {
-  for (let i = 0; i < 10; i++) {
-    let pos = { 
-      x: Math.floor(Math.random() * 1400),
-      y: Math.floor(Math.random() * 750),
-    };
-
-    let tree = new Tree(i, pos, 25);
-    Tree.list[i] = tree;
-  }
-}
-
-Tree.update = () => {
-  const pack = [];
-
-  for (let i in Tree.list) {
-    const tree = Tree.list[i];
-    tree.update();
-    pack.push(tree)
-  }
-
-  return pack;
-}
-
-// render at 60fps via setInterval
-// emits all updates for all players
-
+// game.js?
+// Ghost.spawnGhosts();
 Tree.spawnTrees();
 
 setInterval(() => {
+  // pass entities to all?
   const pack = {
-    player: Player.update(),
+    player: Player.update(entities.trees),
     tree: Tree.update()
-  }
+  };
 
   for (let i in socketList) {
     const socket = socketList[i];
--- player.js
+++ player.js
@@ -1,34 +1,61 @@
 const Entity = require("./entity");
 
-// this.state = ['NEUTRAL', 'FETAL', 'TORCH', 'LOG', 'STICK', 'HEALING', 'BEINGHEALED']
-
 class Player extends Entity {
   constructor(id, pos, size) {
     super(id, pos, size);
+    this.state = "NEUTRAL";
+    this.speed = 3;
     this.pressingRight = false;
     this.pressingLeft = false;
     this.pressingUp = false;
     this.pressingDown = false;
-    this.state = "NEUTRAL";
-    this.speed = 10
-  };
+  }
 
-  // nice
+  static update(treeList) {
+    const pack = [];
+
+    for (let i in Player.list) {
+      let player = Player.list[i];
+      player.updatePosition(treeList);
+      pack.push(player);
+    }
+
+    return pack;
+  }
+
   update() {
     super.update();
     this.updatePosition();
   }
 
-  updatePosition() {
-    if (['NEUTRAL', 'RUNNING', 'CARRYTORCH', 'CARRYLOG'].includes(this.state)) {
-      if (this.pressingRight) this.x += this.speed;
-      if (this.pressingLeft) this.x -= this.speed;
-      if (this.pressingUp) this.y -= this.speed;
-      if (this.pressingDown) this.y += this.speed;
-    }
-  };
+  updatePosition(treeList) {
+    let tempPos = { x: this.x, y: this.y };
 
+    if (this.pressingRight) tempPos.x += this.speed;
+    if (this.pressingLeft) tempPos.x -= this.speed;
+    if (this.pressingDown) tempPos.y += this.speed;
+    if (this.pressingUp) tempPos.y -= this.speed;
+
+    if (!this.playerTreeCollision(tempPos, treeList)) {
+      this.x = tempPos.x;
+      this.y = tempPos.y;
+    }
+  }
+
+  playerTreeCollision(tempPos, treeList) {
+    return Object.values(treeList).some(tree => {
+      const dx = tempPos.x - tree.x;
+      const dy = tempPos.y - tree.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + tree.size) {
+        return true;
+      }
+    });
+  }
 };
+
+Player.list = {};
 
 // change to webpack?
 module.exports = Player;
