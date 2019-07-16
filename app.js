abe1faeaba3c31546b2bc90b29eaf2981931c210
spark <ses8621@gmail.com>
2019-07-16 09:07:07 -0700

    Implement chat feature

--- app.js
+++ app.js
@@ -73,6 +73,14 @@ Player.onDisconnect = socket => {
   delete Player.list[socket.id];
 };
 
+
+
+// game.js?
+// Ghost.spawnGhosts();
+
+Tree.spawnTrees();
+
+
 // on client "connection", do the following
 io.on("connection", socket => {
   socket.id = Math.random();
@@ -96,12 +104,15 @@ io.on("connection", socket => {
 
     Player.onDisconnect(socket);
   });
+
+  socket.on("sendMsgToServer", (data) => {
+    let playerName = ("" + socket.id).slice(2, 7);
+    for (let i in socketList) {
+      socketList[i].emit("addToChat", playerName + ": " + data);
+    }
+  })
 });
 
-// game.js?
-// Ghost.spawnGhosts();
-
-Tree.spawnTrees();
 
 let count = 0
 
