fd6a094f82e8db90ea2933b76175370c4962c8a3
spark <ses8621@gmail.com>
2019-07-17 07:52:22 -0700

    initial commit for fog. bgimage not showing up.

--- fire.js
+++ fire.js
@@ -49,7 +49,7 @@ class Fire extends Entity {
     if(this.firePower > 1){
       this.firePower -= 1;
       this.updateRadius();
-      console.log(`Dwindling ` + `${this.firePower}`);
+      // console.log(`Dwindling ` + `${this.firePower}`);
 
       if (this.firePower > 60) this.status = "HIGH"
       if (this.firePower > 50) this.status = "MEDIUM"
