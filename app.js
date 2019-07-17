3ce2be30b29c785a7fe54d95677e45bcc943a7d1
spark <ses8621@gmail.com>
2019-07-17 13:16:32 -0700

    heroku update master

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ const spawner2 = new Stalker(0, { x: 1, y: 1 }, 15);
 
 
 if (process.env.NODE_ENV === 'production') {
+  app.use("/client", express.static(__dirname + "/client"));
+  server.listen(port);
+
+
   app.get("/", (req, res) => {
     res.sendFile(__dirname + "/client/start_page.html");
   });
@@ -30,10 +34,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get("/game", (req, res) => {
     res.sendFile(__dirname + "/client/index.html");
   });
-
-
-  app.use("/client", express.static(__dirname + "/client"));
-  server.listen(port);
+  // app.use("/client", express.static(__dirname + "/client"));
+  // app.get("/", (req, res) => {
+  //   res.sendFile(__dirname + "/client/index.html");
+  // });
 }
   
 app.get("/", (req, res) => {
